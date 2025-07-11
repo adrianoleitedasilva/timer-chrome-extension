0dcff6578660e46661272ea60664ff68f82e62e2
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2025-07-10 22:10:34 -0300

    Adicionando storage

--- options.js
+++ options.js
@@ -2,5 +2,15 @@ const nameInput = document.getElementById("name-input")
 const saveBtn = document.getElementById("save-btn")
 
 saveBtn.addEventListener("click", () => {
-    console.log(nameInput.value)
+    const name = nameInput.value
+    chrome.storage.sync.set({
+        name,
+    }, () => {
+        console.log(`Nome armazenado foi de ${name}`)
+    })
+})
+
+chrome.storage.sync.get(["name", "test"], (res) => {
+    nameInput.value = res.name ?? "???"
+    console.log(res)
 })
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const timeElement = document.getElementById("time")
+const nameElement = document.getElementById("name")
 const currentTime = new Date().toLocaleTimeString()
 timeElement.textContent = `O Tempo está em: ${currentTime}`
 
@@ -6,4 +7,9 @@ chrome.action.setBadgeText({
     text: "Time",
 }, () => {
     console.log("Finalizado!")
+})
+
+chrome.storage.sync.get(["name"], (res) => {
+    const name = res.name ?? "???"
+    nameElement.textContent = `Seu nome é: ${name}`
 })
